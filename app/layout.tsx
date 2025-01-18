b6541657ef2c66b5b2721323cfba43a566757e64
magnolia-AI <github@antonmagnusson.se>
2025-01-18 15:50:02 +0100

    Update 61 files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
   import type { Metadata } from 'next'
+  import { Toaster } from "@/components/ui/toaster"
   import { Inter } from 'next/font/google'
   
   const inter = Inter({ subsets: ['latin'] })
